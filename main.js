a0a3ee49aaae4ba65f724efa1a9d160e92250d1c
protondk <edgarjosue2911@gmail.com>
2023-01-26 16:28:23 -0600

    Solved fizzbuzz for Hackerrank

---
+++ main.js
@@ -0,0 +1,47 @@
+//Solution 1
+
+for (let i = 1; i <= 100; i++) {
+  let result = "";
+
+  if (i % 3 == 0) {
+    result += "Fizz";
+  }
+
+  if (i % 5 == 0) {
+    result += "Buzz";
+  }
+
+  console.log(result || i);
+}
+
+//Hackerrank
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(5);
+
+//Hackerrank all tests passed
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
